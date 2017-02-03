23c32e9fd2f6da3e161143eb32347011bda93c73
Kevin Turney <super996@optonline.net>
2017-02-03 14:37:49 -0500

    add .env, style header.ejs, include partial in index, added materialize

--- db/models/user.js
+++ db/models/user.js
@@ -1,18 +1,20 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING(32),
-    password: DataTypes.TEXT,
-    email: DataTypes.STRING(128),
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    dob: DataTypes.DATE
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return User;
+   var User = sequelize.define('User', {
+       username: {
+           type: DataTypes.STRING(32),
+           unique: true
+       },
+       password: DataTypes.TEXT,
+       email: DataTypes.STRING(128),
+       firstName: DataTypes.STRING,
+       lastName: DataTypes.STRING
+   }, {
+       classMethods: {
+           associate: function(models) {
+               // associations can be defined here
+           }
+       }
+   });
+   return User;
 };
