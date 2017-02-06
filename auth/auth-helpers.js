421198fde68f04dfbf2f8dffc665bec65f7912ab
Steven Gallo <steven.gallo@outook.com>
2017-02-06 15:02:29 -0500

    delete

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -24,18 +24,11 @@ function createUser(req, res) {
     });
 }
 
-// function loginRequired(req, res, next) {
-//     if (!req.user) res.redirect('/auth/login');
-
-//     return next();
-// }
-
 function loginRequired(req, res, next) {
-  if(!req.user) {
-    return res.status(401).json(
-    { status: 'Please log in'});
-  }
-  return next();
+    if (!req.user) {
+        return res.status(401).json({ status: 'Please log in' });
+    }
+    return next();
 }
 
 module.exports = {
